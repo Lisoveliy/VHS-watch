8c3305685fd43f5fe164e16066793069d705859f
Савелий Савенок <56991906+Lisoveliy@users.noreply.github.com>
2024-10-27 19:08:24 +0300

    fix(release): fixed info update on battery, heartrate and distance widget

--- watchface/modules/battery.js
+++ watchface/modules/battery.js
@@ -1,36 +1,24 @@
 import { getScene, SCENE_AOD } from '@zos/app'
-import * as hmSensor from '@zos/sensor'
 import * as hmUI from '@zos/ui'
 import fontArray from '../fontData/secondaryFont'
 
-let batteryWg;
 const moduleX = 310
 const moduleY = 10
 export default function () {
-  console.log("[modules]: battery module init")
-  let battery = new hmSensor.Battery()
-  updateBatteryWidget(battery.getCurrent())
-  battery.onChange(() => updateBatteryWidget(battery.getCurrent()))
-  if (getScene() != SCENE_AOD)
-    hmUI.createWidget(hmUI.widget.IMG, {
-      x: moduleX,
-      y: moduleY,
-      src: 'date/perc.PNG'
-    })
-}
-function updateBatteryWidget(batteryPercentage) {
-  if (batteryWg)
-    hmUI.deleteWidget(batteryWg)
-
-  if (getScene() != SCENE_AOD) {
-    batteryWg = hmUI.createWidget(hmUI.widget.TEXT_IMG, {
-      x: moduleX - 85,
-      y: moduleY,
-      type: hmUI.data_type.BATTERY,
-      font_array: fontArray,
-      h_space: 1,
-      align_h: hmUI.align.RIGHT,
-      text: batteryPercentage
-    })
-  }
+	console.log("[modules]: battery module init")
+	if (getScene() != SCENE_AOD) {
+		hmUI.createWidget(hmUI.widget.IMG, {
+			x: moduleX,
+			y: moduleY,
+			src: 'date/perc.PNG'
+		})
+		hmUI.createWidget(hmUI.widget.TEXT_IMG, {
+			x: moduleX - 85,
+			y: moduleY,
+			font_array: fontArray,
+			h_space: 1,
+			align_h: hmUI.align.RIGHT,
+			type: hmUI.data_type.BATTERY
+		})
+	}
 }
--- watchface/modules/distance.js
+++ watchface/modules/distance.js
@@ -1,41 +1,24 @@
 import { getScene, SCENE_AOD } from '@zos/app'
-import * as hmSensor from '@zos/sensor'
 import * as hmUI from '@zos/ui';
 import secondaryFont from '../fontData/secondaryFont';
 
-let distWg;
 const moduleX = 50
 const moduleY = 405
 export default function () {
     console.log("[modules]: distance module init")
-    let distance = new hmSensor.Distance();
-    updateDistance(distance.getCurrent())
-    distance.onChange(() => updateDistance(distance.getCurrent()))
     if (getScene() != SCENE_AOD) {
         hmUI.createWidget(hmUI.widget.IMG, {
             x: moduleX,
             y: moduleY,
             src: 'misc/dist.PNG'
         })
-    }
-}
-
-function updateDistance(dist) {
-    if (!dist)
-        dist = 0
-
-    if (distWg)
-        hmUI.deleteWidget(distWg)
-
-    if (getScene() != SCENE_AOD) {
-        distWg = hmUI.createWidget(hmUI.widget.TEXT_IMG, {
+        hmUI.createWidget(hmUI.widget.TEXT_IMG, {
             x: moduleX + 150,
             y: moduleY,
-            type: hmUI.data_type.DISTANCE,
             font_array: secondaryFont,
             h_space: 1,
             align_h: hmUI.align.LEFT,
-            text: dist
+            type: hmUI.data_type.DISTANCE
         })
     }
 }
